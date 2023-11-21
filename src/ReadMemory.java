5c71707528abcb67f3198ced8dfd261738c081a0
jundizhou <jundizhou@harmonycloud.cn>
2023-11-21 16:10:41 +0800

    first commit

--- src/ReadMemory.java
+++ src/ReadMemory.java
@@ -1,60 +1,78 @@
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.nio.charset.StandardCharsets;
 
 public class ReadMemory {
     String fileName = "shm.lock";
     RandomAccessFile raFile;
-    FileChannel fc;
-    int iSize = 1024;
+    static FileChannel fc;
     MappedByteBuffer mapBuf;
-    int iMode;
-    int lastIndex = 0;
+    int fileSize = 0;
 
-    public ReadMemory() {
+
+    public void initMemFile(String fileName, int size){
         try {
-            init();
-        } catch (Exception e) {
-            e.printStackTrace();
+            raFile = new RandomAccessFile(fileName, "rw");
+            fc = raFile.getChannel();
+            mapBuf = fc.map(FileChannel.MapMode.READ_WRITE, 0, size);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-    }
-
-    public void init() throws Exception {
-        raFile = new RandomAccessFile(fileName, "rw");
-        fc = raFile.getChannel();
-        mapBuf = fc.map(FileChannel.MapMode.READ_WRITE, 0, iSize);
     }
 
     public void clearBuffer() {
         // 清除文件内容
-        for (int i = 0; i < 1024; i++) {
+        for (int i = 0; i < fileSize; i++) {
             mapBuf.put(i, (byte) 0);
         }
+        mapBuf.position(0);
+    }
+
+
+
+    public String readMemFile() throws IOException {
+
+        FileLock lock = null;
+        try {
+            lock = fc.tryLock();
+        } catch (IOException e) {
+            lock.release();
+            e.printStackTrace();
+        }
+        if (lock != null) {
+            try {
+                getBuffer();
+                lock.release();
+            } catch (Exception e) {
+                lock.release();
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("can't get lock");
+        }
+        return "ok";
     }
 
     public void getBuffer() throws Exception{
-        for(int i=1;i<27;i++){
-            int flag = mapBuf.get(0); //取读写数据的标志
-            int index = mapBuf.get(1); //读取数据的位置,2 为可读
-
-            if(flag != 2 || index == lastIndex){ //假如不可读，或未写入新数据时重复循环
-                i--;
-                continue;
-            }
-
-            lastIndex = index;
-            System.out.println("程序 ReadShareMemory：" + System.currentTimeMillis() +
-                    "：位置：" + index +" 读出数据：" + (char)mapBuf.get(index));
-
-            mapBuf.put(0,(byte)0); //置第一个字节为可读标志为 0
-
-            if(index == 27){ //读完数据后退出
+        //mapBuf.put(0,(byte)2);
+        while (mapBuf.hasRemaining()) {
+            int length = mapBuf.getInt(); // 读取数据长度元数据
+            if(length == 0){
                 break;
             }
+            byte[] readData = new byte[length];
+            System.out.println(length);
+            mapBuf.get(readData); // 读取实际数据
+            System.out.println(new String(readData)); // 输出数据
         }
+        //mapBuf.put(0,(byte)0);
     }
 
     public boolean getLock() {
@@ -71,17 +89,15 @@ public class ReadMemory {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         // TODO Auto-generated method stub
-        ReadMemory map = new ReadMemory();
-        if (map.getLock()) {
-            try {
-                map.getBuffer();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("can't get lock");
+        ReadMemory readMemory = new ReadMemory();
+        readMemory.initMemFile("shm.lock",  8 * 1024 * 1024);
+        while (true) {
+            readMemory.readMemFile();
+            readMemory.clearBuffer();
+            Thread.sleep(5000);
         }
+
     }
 }
--- src/WriteMemory.java
+++ src/WriteMemory.java
@@ -1,2 +1,107 @@
-package PACKAGE_NAME;public class WriteMemory {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.util.Random;
+import java.nio.charset.StandardCharsets;
+
+public class WriteMemory {
+    RandomAccessFile raFile;
+    FileChannel fc;
+    MappedByteBuffer mapBuf;
+    int fileSize = 0;
+    int currentIndex = 1;
+
+    public boolean initMemFile(String fileName, int size) {
+        try {
+            raFile = new RandomAccessFile(fileName, "rw");
+            fc = raFile.getChannel();
+            mapBuf = fc.map(FileChannel.MapMode.READ_WRITE, 0, size);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < size; i++) {
+            mapBuf.put(i, (byte) 0);
+        }
+        fileSize = size;
+        return true;
+    }
+
+    public String writeMemFile(int length, byte[] content) throws IOException {
+        FileLock lock = null;
+        try {
+            lock = fc.tryLock();
+        } catch (IOException e) {
+            lock.release();
+            e.printStackTrace();
+        }
+        if (lock != null) {
+            try {
+                putBuffer(length, content);
+                lock.release();
+            } catch (Exception e) {
+                lock.release();
+                e.printStackTrace();
+            }
+        } else {
+            // TODO: 重试
+            System.out.println("can't get lock");
+        }
+        return "ok";
+    }
+
+
+    // index = 0为标志位，0为空闲，1为正在写，2为正在读。预留,如果后续tryFileLock性能不行可以用此标志位来控制并发读写
+    public void putBuffer(int length, byte[] content){
+        if(currentIndex+length>=fileSize) {
+            System.out.println("drop event!");
+            return;
+        }
+        //mapBuf.put(0, (byte) 1);
+        mapBuf.putInt(length);
+        mapBuf.put(content);
+        currentIndex = currentIndex + length;
+        //mapBuf.put(0, (byte) 0);
+    }
+
+
+
+
+
+
+    // 以下为测试代码
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // TODO Auto-generated method stub
+        WriteMemory writeMemory = new WriteMemory();
+        writeMemory.initMemFile("shm.lock",  8 * 1024 * 1024);
+        while (true) {
+            String randomString = generateRandomString();
+            byte[] bytes = randomString.getBytes(StandardCharsets.UTF_8);
+            System.out.println("start write: " + new String(bytes));
+            System.out.println("length: " + bytes.length);
+            writeMemory.writeMemFile(bytes.length, bytes);
+            Thread.sleep(1000);
+        }
+
+    }
+    private static String generateRandomString() {
+        Random random = new Random();
+        int length = random.nextInt(500) + 1; // 生成 1 到 500 之间的随机长度
+        StringBuilder sb = new StringBuilder();
+
+        // 生成随机字符串
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) (random.nextInt(26) + 'a'); // 生成随机字符，这里以小写字母为例
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+
 }
